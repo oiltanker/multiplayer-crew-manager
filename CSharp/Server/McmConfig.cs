67a49f08fca984829242c5d8bd8bc1faabbf98f6
Akronyhm <MarcusFlysjo@gmail.com>
2023-10-22 17:36:27 +0200

    Edited the LoadConfig function because it was causing the mod to crash intermittently on compilation

--- CSharp/Server/McmConfig.cs
+++ CSharp/Server/McmConfig.cs
@@ -1,30 +1,70 @@
 using System;
+using System.Xml.Serialization;
 using Barotrauma;
 
-namespace MultiplayerCrewManager {
-    partial class McmMod {
-        public class McmConfig {
-            public int ServerUpdateFrequency = 15;
-            public bool AllowSpawnNewClients = false;
-            public bool AllowRespawns = false;
-            public bool SecureEnabled = false;
-            public bool RespawnPenalty = true;
-            public float RespawnTime = 180;
-            public float RespawnDelay = 5;
+namespace MultiplayerCrewManager
+{
+    public class McmConfig
+    {
+        public int ServerUpdateFrequency = 15;
+        public bool AllowSpawnNewClients = false;
+        public bool AllowRespawns = false;
+        public bool SecureEnabled = false;
+        public bool RespawnPenalty = true;
+        public float RespawnTime = 180;
+        public float RespawnDelay = 5;
 
-            public McmConfig() { }
-        }
+        public McmConfig() { }
+    }
 
-        public static string GetConfigFilePath => System.IO.Path.Combine(ACsMod.GetStoreFolder<McmMod>(), "McmConfig.xml");
+    partial class McmMod
+    {
+
         public static McmConfig Config { get; private set; }
 
-        public static void LoadConfig() {
-            if (LuaCsFile.Exists(GetConfigFilePath))
-                Config = LuaCsConfig.Load<McmConfig>(GetConfigFilePath);
-            else Config = new McmConfig();
+        public static void LoadConfig()
+        {
+            var cfgFilePath = System.IO.Path.Combine(ACsMod.GetStoreFolder<McmMod>(), "McmConfig.xml");
+            try
+            {
+                var serializer = new XmlSerializer(typeof(McmConfig));
+                using (var fstream = System.IO.File.OpenRead(cfgFilePath))
+                {
+                    Config = (McmConfig)serializer.Deserialize(fstream);
+                }
+                #region Old code disabled as LuaCsConfig kept crashing when attempting to load the config file
+                //if (false == string.IsNullOrWhiteSpace(cfgFilePath) && LuaCsFile.Exists(cfgFilePath))
+                //    Config = (McmConfig)LuaCsConfig.Load<object>(cfgFilePath);
+                //else Config = new McmConfig();
+                #endregion
+            }
+            catch (System.Exception e)
+            {
+                LuaCsSetup.PrintCsMessage($"[MCM-SERVER] Loading config ERROR : [{e.Message}]");
+                Config = new McmConfig();
+            }
         }
-        public static void SaveConfig() {
-            LuaCsConfig.Save(GetConfigFilePath, Config);
+        public static void SaveConfig()
+        {
+            var cfgFilePath = System.IO.Path.Combine(ACsMod.GetStoreFolder<McmMod>(), "McmConfig.xml");
+            if (false == string.IsNullOrWhiteSpace(cfgFilePath))
+            {
+                #region Directly serialize config file using xmlserializer instead of using LuaCsConfig as it caused problems when attempting to deserialize the config file afterwards
+                //LuaCsConfig.Save(cfgFilePath, Config);
+                #endregion
+                try
+                {
+                    var serializer = new XmlSerializer(typeof(McmConfig));
+                    using (var fstream = new System.IO.StreamWriter(cfgFilePath))
+                    {
+                        serializer.Serialize(fstream, Config);
+                    }
+                }
+                catch (Exception e)
+                {
+                    LuaCsSetup.PrintCsMessage($"[MCM-SERVER] Saving config ERROR : [{e.Message}]");
+                }
+            }
         }
     }
 }
